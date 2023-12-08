3aeb7ef7df1c9f4dfb3e7bc11157adfb89eafacc
Karolina Mäkinen <karolina.makinen1@gmail.com>
2023-12-09 00:25:45 +0200

    Osa 3 liityviä muutoksia

--- osa2/puhelinluettelo/src/components/persons.jsx
+++ osa2/puhelinluettelo/src/components/persons.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const Persons = () => {
 
     axios
-        .get('http://localhost:3001/persons')
+        .get('http://localhost:3001/api/persons')
         .then(response => {
             const persons = response.data
             console.log(persons)
