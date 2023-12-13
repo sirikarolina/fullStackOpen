4e290f86e7efcf4d5edfb67c49eae494b36db4bc
Karolina Mäkinen <karolina.makinen1@gmail.com>
2023-12-13 14:25:24 +0200

    osa4 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,29 +1,45 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
+  
+  const user = request.user
+
+	if(!user){
+		return response.status(401).json({ error: 'token missing or invalid' })  
+	}
 
 	const blog = new Blog({
 		title: body.title,
 		author: body.author,
 		url: body.url,
 		likes: body.likes? body.likes : 0,
+    user: user._id
 	})
 
 	if(body.title === undefined || body.url === undefined){
 		response.status(400).end()
-	}else{
-		const savedBlog = await blog.save()    
-		response.status(201).json(savedBlog)
-	}
-	
-	  
+	} else {
+		const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save() 
+	} 
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -36,8 +52,19 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+	const user = request.user
+
+	if(!user){
+		return response.status(401).json({ error: 'token missing or invalid' })  
+	}
+
+	const blog = await Blog.findById(request.params.id)
+	if(blog.user.toString() === request.user.id){
+		await Blog.findByIdAndRemove(request.params.id)
+	    response.status(204).end()
+	}else{
+		return response.status(401).json({ error: 'Unauthorized to delete the blog' })
+	}
 })
 
 blogsRouter.put('/:id', async (request, response) => {
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
-beforeEach(async () => {  
-  await Blog.deleteMany({})
-
-  const blogObjects = helper.initialBlogs
-    .map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
-}, 100000)
-
 describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -28,119 +27,158 @@ describe('when there is initially some blogs saved', () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
 
-  test('the unique identifier property of the blog posts is named id', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-  
-    const blogToView = blogsAtStart[0]
-  
-    const resultBlog = await api    
-      .get(`/api/blogs/${blogToView.id}`)    
-      .expect(200)    
-      .expect('Content-Type', /application\/json/)
-  
-    expect(resultBlog).toBeDefined()
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.content)
+    expect(contents).toContain(
+      'Browser can execute only JavaScript'
+    )
+  })
+
+  describe('viewing a specific blog', () => {
+
+    test('succeeds with a valid id', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+
+      const blogToView = blogsAtStart[0]
+
+      const resultblog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultblog.body).toEqual(blogToView)
+    })
+
+    test('fails with statuscode 404 if blog does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api
+        .get(`/api/blogs/${validNonexistingId}`)
+        .expect(404)
+    })
+
+    test('fails with statuscode 400 id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
+
+      await api
+        .get(`/api/blogs/${invalidId}`)
+        .expect(400)
+    })
+  })
+
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newblog = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newblog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+      const contents = blogsAtEnd.map(n => n.content)
+      expect(contents).toContain(
+        'async/await simplifies making async calls'
+      )
+    })
+
+    test('fails with status code 400 if data invalid', async () => {
+      const newblog = {
+        important: true
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newblog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+      )
+
+      const contents = blogsAtEnd.map(r => r.content)
+
+      expect(contents).not.toContain(blogToDelete.content)
+    })
   })
 })
 
-describe('viewing a specific blog', () => {
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2
-    }  
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'kiila96',
+      name: 'Karolina',
+      password: 'salainen',
+    }
 
     await api
-      .post('/api/blogs')
-      .send(newBlog)
+      .post('/api/users')
+      .send(newUser)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
 
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
 
-    const titles = blogsAtEnd.map(n => n.title)
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
 
-    expect(titles).toContain(
-      'Type wars'
-    )
-  }, 100000)
-
-  test('new blog without likes property will be set to 0', async () => {
-    const newBlog = {
-      title: 'Typefghfhgfhg wars',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWarsgfghfhgf.html',
-      author: 'Robertdsjflkjfslkd C. Martin',
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
     }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
       .expect('Content-Type', /application\/json/)
 
-      const blogsAtEnd = await helper.blogsInDb()
+    expect(result.body.error).toContain('expected `username` to be unique')
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-    
-      const likeLikes = blogsAtEnd.map(n => n.likes)
-    
-      expect(likeLikes).toContain(0)
-  }) 
-
-  test('new blog without title property will not be added', async () => {
-    const newBlog = {
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/Typsgfghfhgf.html',
-      author: 'Robertdsjflkd C. Martin',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-      const blogsAtEnd = await helper.blogsInDb()
-
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-  }) 
-
-  test('new blog without url property will not be added', async () => {
-    const newBlog = {
-      title: 'dgfshjdfgsjdh',
-      author: 'Robertdsjflkd C. Martin'
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-      const blogsAtEnd = await helper.blogsInDb()
-
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-  }) 
-})
-
-describe('deletion of a blog', () => {
-  test('a blog can be deleted', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
-    await api    
-      .delete(`/api/blogs/${blogToDelete.id}`)    
-      .expect(204)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(
-      helper.initialBlogs.length - 1
-    )
-
-    const titles = blogsAtEnd.map(r => r.title)
-
-    expect(titles).not.toContain(blogToDelete.title)
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
